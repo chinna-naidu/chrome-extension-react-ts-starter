347ef4eb93cf0fa6978d0c0adbd5f6ae9e203abb
chinna-naidu <51732781+chinna-naidu@users.noreply.github.com>
2021-11-28 14:45:26 +0530

    added rules for processing assets in webpack
    added rules to process images, fonts using webpack

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,10 @@ module.exports = {
         test: /.css/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+        type: 'asset/resource'
+      }
     ],
   },
   resolve: {
